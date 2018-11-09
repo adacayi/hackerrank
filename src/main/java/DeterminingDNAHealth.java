d33158793d4c3d8c5d88e61c2ad726e1e400a9c4
Abdullah Sanver <a_sanver@hotmail.com>
2018-11-09 08:42:45 +0000

    DeterminingDNAHealth added with slow performance.

---
+++ src/main/java/DeterminingDNAHealth.java
@@ -0,0 +1,196 @@
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class DeterminingDNAHealth {
+    public static Gene[] getGenes(String[] geneStrings, int[] healths) {
+        int i, n = geneStrings.length;
+        Gene[] result = new Gene[n];
+        for (i = 0; i < n; i++)
+            result[i] = new Gene(geneStrings[i], healths[i], false, i);
+        Arrays.sort(result, Comparator.nullsFirst(Comparator.comparing((Gene x) ->
+                x == null || x.name == null || x.name.length == 0 ? null : x.name[0])).thenComparing(Gene::getIndex));
+        for (i = 0; i < n - 1; i++)
+            if (Arrays.equals(result[i].name, result[i + 1].name))
+                result[i].isNextSame = true;
+
+        return result;
+    }
+
+    public static class Gene {
+        private char[] name;
+        private int health;
+        private boolean isNextSame;
+        private int index;
+
+        public char[] getName() {
+            return name;
+        }
+
+        public int getHealth() {
+            return health;
+        }
+
+        public boolean isNextSame() {
+            return isNextSame;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setNextSame(boolean nextSame) {
+            isNextSame = nextSame;
+        }
+
+        public Gene(String name, int health, boolean isNextSame, int index) {
+            this.name = name.toCharArray();
+            this.health = health;
+            this.isNextSame = isNextSame;
+            this.index = index;
+        }
+
+        @Override
+        public int hashCode() {
+            Field[] fields = this.getClass().getDeclaredFields();
+            int i, n = fields.length;
+            Object[] values = new Object[n];
+            Object obj;
+            for (i = 0; i < n; i++) {
+                try {
+                    obj = fields[i].get(this);
+                    if (fields[i].getType().isArray()) {
+                        values[i] = arrayHashCode(obj);
+                    } else
+                        values[i] = obj;
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            return Arrays.hashCode(values);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!this.getClass().isInstance(obj))
+                return false;
+
+            Field[] fields = this.getClass().getDeclaredFields();
+            Object first, second;
+            for (Field field : fields) {
+                try {
+                    first = field.get(this);
+                    second = field.get(obj);
+                    if (field.getType().isArray()) {
+                        if (!Objects.deepEquals(first, second))
+                            return false;
+                    } else if (!Objects.equals(first, second))
+                        return false;
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
+    }
+
+    public static int arrayHashCode(Object obj) {
+        if (obj instanceof Object[])
+            return Arrays.deepHashCode((Object[]) obj);
+        if (obj instanceof byte[])
+            return Arrays.hashCode((byte[]) obj);
+        if (obj instanceof short[])
+            return Arrays.hashCode((short[]) obj);
+        if (obj instanceof int[])
+            return Arrays.hashCode((int[]) obj);
+        if (obj instanceof long[])
+            return Arrays.hashCode((long[]) obj);
+        if (obj instanceof char[])
+            return Arrays.hashCode((char[]) obj);
+        if (obj instanceof float[])
+            return Arrays.hashCode((float[]) obj);
+        if (obj instanceof double[])
+            return Arrays.hashCode((double[]) obj);
+        return Arrays.hashCode((boolean[]) obj);
+    }
+
+    public static void main(String... args) {
+        int i, m, start, end;
+        long min = Long.MAX_VALUE, max = 0, current;
+        String strand;
+        Gene[] genes;
+        String[] geneStrings;
+        int[] healths;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            scanner.nextInt();
+            scanner.nextLine();
+            geneStrings = scanner.nextLine().split("\\s");
+            healths = Arrays.stream(scanner.nextLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+            m = scanner.nextInt();
+            genes = getGenes(geneStrings, healths);
+
+            for (i = 0; i < m; i++) {
+                start = scanner.nextInt();
+                end = scanner.nextInt();
+                strand = scanner.next();
+                current = findHealth(start, end, strand, genes);
+                min = Math.min(min, current);
+                max = Math.max(max, current);
+            }
+            System.out.printf("%d %d", min, max);
+        }
+    }
+
+    public static long findHealth(int start, int end, String strand, Gene[] genes) {
+        long value = 0;
+        int i, j, k, l = strand.length(), n = genes.length, index;
+        char[] strandArray = strand.toCharArray();
+        char first;
+        Gene gene;
+        for (i = 0; i < l; i++) {
+            first = strandArray[i];
+            index = binarySearch(genes, 0, n, first);
+            if (index < 0)
+                continue;
+            for (j = index; j < n && genes[j].name[0] == first; j++) {
+                gene = genes[j];
+
+                if (start > gene.index || end < gene.index)
+                    continue;
+                for (k = 0; k < gene.name.length && i + k < l && strandArray[i + k] == gene.name[k]; k++) ;
+                if (k != gene.name.length)
+                    continue;
+
+                do {
+                    value += genes[j].health;
+                    j++;
+                } while (j < n && genes[j - 1].isNextSame);
+                j--;
+            }
+        }
+
+        return value;
+    }
+
+    private static int binarySearch(Gene[] genes, int fromIndex, int toIndex, char key) {
+        int low = fromIndex, high = toIndex - 1, mid, result = -1;
+        char midVal;
+        while (low <= high) {
+            mid = (low + high) >>> 1;
+            midVal = genes[mid].name[0];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+                if (midVal == key)
+                    result = mid;
+            }
+        }
+
+        if (result > -1)
+            return result;
+
+        return ~low;
+    }
+}
---
+++ src/test/java/DeterminingDNAHealthTest.java
@@ -0,0 +1,117 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class DeterminingDNAHealthTest {
+    private static final PrintStream originalPrintStream = System.out;
+    private static final InputStream originalInputStream = System.in;
+    private ByteArrayOutputStream outputStream;
+    private ByteArrayInputStream inputStream;
+
+    @Before
+    public void init() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalPrintStream);
+        System.setIn(originalInputStream);
+
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void should_Return_0_19_When_Main() {
+        String input = "6\n" +
+                "a b c aa d b\n" +
+                "1 2 3 4 5 6\n" +
+                "3\n" +
+                "1 5 caaab\n" +
+                "0 4 xyz\n" +
+                "2 4 bcdybc";
+        inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+        DeterminingDNAHealth.main();
+        String expected = "0 19";
+        String actual = outputStream.toString().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_Return_1823728075410_1823728075410() throws IOException, URISyntaxException {
+        Path inputPath = Paths.get(this.getClass().getResource("DeterminingDNAHealthInput.txt").toURI());
+        try (InputStream in = Files.newInputStream(inputPath)) {
+            System.setIn(in);
+            DeterminingDNAHealth.main();
+            String expected = "1823728075410 1823728075410";
+            String actual = outputStream.toString().trim();
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void should_Return_3218660_11137051() throws IOException, URISyntaxException {
+        Path inputPath = Paths.get(this.getClass().getResource("DeterminingDNAHealthInput2.txt").toURI());
+        try (InputStream in = Files.newInputStream(inputPath)) {
+            System.setIn(in);
+            DeterminingDNAHealth.main();
+            String expected = "3218660 11137051";
+            String actual = outputStream.toString().trim();
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void should_Return_19_When_Find_Health() {
+        long expected = 19;
+        DeterminingDNAHealth.Gene[] genes = DeterminingDNAHealth.getGenes(
+                new String[]{"a", "b", "c", "aa", "d", "b"}, new int[]{1, 2, 3, 4, 5, 6});
+        long actual = DeterminingDNAHealth.findHealth(1, 5, "caaab",
+                genes);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void should_Get_Sorted_Genes() {
+        String[] geneStrings = {"a", "b", "c", "aa", "d", "b"};
+        int[] healths = {1, 2, 3, 4, 5, 6};
+        DeterminingDNAHealth.Gene[] expected = {
+                new DeterminingDNAHealth.Gene("a", 1, false, 0)
+                , new DeterminingDNAHealth.Gene("aa", 4, false, 3)
+                , new DeterminingDNAHealth.Gene("b", 2, true, 1)
+                , new DeterminingDNAHealth.Gene("b", 6, false, 5)
+                , new DeterminingDNAHealth.Gene("c", 3, false, 2)
+                , new DeterminingDNAHealth.Gene("d", 5, false, 4)
+        };
+        DeterminingDNAHealth.Gene[] actual = DeterminingDNAHealth.getGenes(geneStrings, healths);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void should_Return_Same_HashCode() {
+        String[] first = {"Ahmet", "Mustafa", "Muhammed"}, second = {"Ahmet", "Mustafa", "Muhammed"};
+        assertEquals(DeterminingDNAHealth.arrayHashCode(first), DeterminingDNAHealth.arrayHashCode(second));
+    }
+}
